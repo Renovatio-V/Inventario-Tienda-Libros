771614cb2bed458be58031a0a0b4661c3c2b6401
L1t1um <L1t1um@DESKTOP-7O7QPGF>
2025-04-06 23:35:45 -0500

    adjust details

--- src/main/java/Renovatio/Tienda_Libros/repositorio/LibroRepositorio.java
+++ src/main/java/Renovatio/Tienda_Libros/repositorio/LibroRepositorio.java
@@ -3,6 +3,7 @@ package Renovatio.Tienda_Libros.repositorio;
 import Renovatio.Tienda_Libros.modelo.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface LibroRepositorio extends JpaRepository<Libro, Integer> {
 
 }
